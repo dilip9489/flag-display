ab607616fc8c51af15e3ebd83e882e9f9a3d4240
dilip <dilip.roy4892@gmail.com>
2024-11-11 02:26:32 +0530

    flag-display

---
+++ src/FlagCard/FlagCard.js
@@ -0,0 +1,37 @@
+import react from "react";
+import { useState, useEffect } from "react";
+import "../FlagCard/FlagCard.css";
+
+const FlagCard = () => {
+  const [flags, setFlags] = useState([]);
+
+  useEffect(() => {
+    const flagData = async () => {
+      try {
+        const res = await fetch(
+          "https://xcountries-backend.azurewebsites.net/all"
+        );
+        const data = await res.json();
+        console.log(data);
+        setFlags(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    flagData();
+  }, []);
+
+  return (
+    <div className="flag-show">
+      {flags.map((flg, idx) => {
+        return (
+          <div key={idx} className="flag-item">
+            <img src={flg.flag} alt={flg.abbr} />
+            <h2>{flg.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default FlagCard;
